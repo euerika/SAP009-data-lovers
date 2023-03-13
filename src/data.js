f95f7af4a2cd57233ff8c65bcc3acb379256a77c
euerika <euerika.soaresdossantos@gmail.com>
2023-03-13 13:39:54 -0300

    Alteração Nova

--- src/data.js
+++ src/data.js
@@ -1,14 +1,12 @@
-export function harryFunçoes(name, listCharacterHouseFilter) {
-  return listCharacterHouseFilter.filter((user) => user.name.includes(name));
+export function harryFunçoes(nome, listaPersonagensFiltrados) {
+  return listaPersonagensFiltrados.filter((harry) => harry.name.includes(nome));
 }
-
 //FILTRO VER TODOS
-export function filterHouse(house, characters) {
-  return characters.filter((user) => user.house === house);
+export function filtrarCasa(casa, personagens) {
+  return personagens.filter((harry) => harry.house === casa);
 }
-
 //ORDENAÇÃO
-export const orderName = (a, b) => {
+export const ordemNome = (a, b) => {
   if (a.name > b.name) {
     return 1;
   }
@@ -16,29 +14,25 @@ export const orderName = (a, b) => {
     return -1;
   }
 };
-
-export const selectNameAz = (characters) => {
-  return characters.sort(orderName);
+export const selecionaNomeAz = (personagens) => {
+  return personagens.sort(ordemNome);
 };
-export const selectNameZa = (characters) => {
-  return characters.sort(orderName).reverse();
+export const selecionaNomeZa = (personagens) => {
+  return personagens.sort(ordemNome).reverse();
 };
-
 //FILTRO POR CASAS
+export const filtrarPersonagemCasa = (personagens, casa) => {
+  const resultadoCasas = personagens.filter(function(personagem) {
+    if (personagem.house === null) {
+      return false
+    }
+    const casaFiltro = personagem.house.includes(casa) //determina se a condição pode ser encontrado dentro da string
+    return casaFiltro
+  })
+  return resultadoCasas
+};
+//CÁLCULO AGREGADO
+export const calcPorcentagem = (todos, parte) => {
+  return ((parte/ todos)).toFixed(2);
+};
 
-//export const filtroCasa = (allPersonagens) => {
-//const resultadoCasas = allPersonagens.filter(function (house) {
-//if (allPersonagens.house === null) {
-//condição para o que for null não apareça no filtro pode usar diferente de null só ?
-//return false;
-//}
-//const casaFiltro = allPersonagens.house.includes(house); //determina se a condição pode ser encontrado dentro da string
-//return casaFiltro;
-//});
-//return resultadoCasas;
-//};
-
-//Recomendamos que este arquivo contenha toda a funcionalidade que corresponda a obter, processar e manipular dados (suas funções):
-//* `filterData(data, condition)`: esta função receberia os dados e nos retornaria os que cumprem com a condição.
-//* `sortData(data, sortBy, sortOrder)`: esta função recebe três parâmetros. O primeiro, `data`, nos entrega os dados. O segundo, `sortBy`, diz respeito a qual das informações quer usar para ordenar. O terceiro, `sortOrder`, indica se quer ordenar de maneira crescente ou decrescente.
-//* `computeStats(data)`: essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,217 @@
-import { example, anotherExample } from '../src/data.js';
+import {
+  selectNameAz,
+  selectNameZa,
+  orderName,
+  calcPorcentagem,
+  filtrarCasa,
+  filtrarPersonagemCasa
+} from "../src/data.js";
 
+const harry = {
+  name: "Harry Potter",
+  house: "Gryffindor",
+};
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const ron = {
+  name: "Ronald Weasley",
+  house: "Gryffindor",
+};
+
+const hermione = {
+  name: "Hermione Granger",
+  house: "Gryffindor",
+};
+
+const avery = {
+  name: "Avery I",
+  house: "Slytherin",
+};
+
+const malcolm = {
+  name: "Malcolm Baddock",
+  house: "Slytherin",
+};
+
+const bloody = {
+  name: "Bloody Baron",
+  house: "Slytherin",
+};
+
+const stewart = {
+  name: "Stewart Ackerley",
+  house: "Ravenclaw",
+};
+
+const marcus = {
+  name: "Marcus Belby",
+  house: "Ravenclaw",
+};
+
+const terry = {
+  name: "Terry Boot",
+  house: "Ravenclaw",
+};
+
+const cadwallader = {
+  name: "Cadwallader",
+  house: "Hufflepuff",
+};
+
+const owen = {
+  name: "Owen Cauldwell",
+  house: "Hufflepuff",
+};
+
+const cedric = {
+  name: "Cedric Diggory",
+  house: "Hufflepuff",
+};
+
+const african = {
+  name: "African prince",
+  house: null,
+};
+
+const albino = {
+  name: "Albino peacock",
+  house: null,
+};
+
+const personagens = [
+  harry,
+  ron,
+  hermione,
+  avery,
+  malcolm,
+  bloody,
+  marcus,
+  stewart,
+  terry,
+  cadwallader,
+  owen,
+  cedric,
+  albino,
+  african,
+];
+
+describe("orderName é uma função", () => {
+  it("is a function", () => {
+    expect(typeof orderName).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  describe("selectNameAz é uma função", () => {
+    it("is a function", () => {
+      expect(typeof selectNameAz).toBe("function");
+    });
+  it("Deve selectNameAz de A-Z", () => {
+    const resultado = selectNameAz(personagens);
+    //expect(resultado).toEqual([
+      african,
+      albino,
+      avery,
+      bloody,
+      cadwallader,
+      cedric,
+      harry,
+      hermione,
+      malcolm,
+      marcus,
+      owen,
+      ron,
+      stewart,
+      terry,
+    //]);
+    expect(resultado[0]).toBe(["African prince"]);
   });
+
+  describe("selectNameZa é uma função", () => {
+    it("is a function", () => {
+      expect(typeof selectNameZa).toBe("function");
+    });
+  it("Deve ordernar de Z-A", () => {
+    const resultado = selectNameZa(personagens);
+    expect(resultado).toEqual([
+      terry,
+      stewart,
+      ron,
+      owen,
+      marcus,
+      malcolm,
+      hermione,
+      harry,
+      cedric,
+      cadwallader,
+      bloody,
+      avery,
+      albino,
+      african,
+    ]);
+  });
+
+//FILTRAR CASA
+describe("filtraCasa é uma função", () => {
+  it("is a function", () => {
+    expect(typeof filtarCasa).toBe("function");
+  });
+  it ("ele retorna um array", () =>{
+    expect( ).tobe(personagens.house)
+  })
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe(" ", () => {
+  it('is a true para ""', () => {
+    expect(filtroCasa("")).toBe(Boolean);
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('is a true para "Hufflepuff"', () => {
+    expect(filtroCasa("Hufflepuff")).toBe(true);
+  });
+
+  it('is a true para "Ravenclaw"', () => {
+    expect(filtroCasa("Ravenclaw")).toBe(true);
+  });
+
+  it('is a retornar false para "Slytherin"', () => {
+    expect(filtroCasa("Slytherin")).toBe(true);
+  });
+
+  it('is a retornar false para "Gryffindor "', () => {
+    expect(filtroCasa("Gryffindor ")).toBe(true);
+  });
+
+  it('is a retornar false para "null"', () => {
+    expect(filtroCasa("null")).toBe(false);
   });
 });
+//CÁLCULO AGREGAD
+// describe("calcPercentage", () => {
+//   it("is a  function ", () => {
+//     expect(typeof calcPorcentagem).toBe("function");
+//   });
+  
+//     it("retorna % de personagens de cada casa selecionadas , quanto é o total?", () => {
+//       const totalDEcasasCasaSelecionada = calculo(dummyData, ["nome", "Gryffindor"]);
+//       expect(totalDEcasasCasaSelecionada).toBe("x % são da casa Gryffindor");
+//     });
+  
+//     it("return total ravenclaw characters", () => {
+//       const totalDEcasasCasaSelecionada = calculo(dummyData, ["nome", "Ravenclaw"]);
+//       expect(totalDEcasasCasaSelecionada).toBe("x % são da casa Ravenclaw");
+       
+//     });
+  
+//     it("return total hufflepuff characters", () => {
+//       const totalDEcasasCasaSelecionada = calculo(dummyData, ["nome", "Hufflepuff"]);
+//       expect(totalDEcasasCasaSelecionada).toBe("x % são da casa Hufflepuff");
+//     });
+  
+//     it("return total slytherin characters", () => {
+//       const totalDEcasasCasaSelecionada = calculo(dummyData, ["nome", "Slytherin"]);
+//       expect(totalDEcasasCasaSelecionada).toBe("x % são da casa Slytherin");
+//     });
+//   });
+
+// //describe('harryFunçoes retornora um array com todos os nome'), () => {
+//   it('harryFunçoes deveria ser uma função', () =>{
+//     expect(typeof harryFunçoes).toBe('function');
+//   });
